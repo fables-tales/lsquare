b528661acf32b60a7530562b74492df35bd72175
Sam Phippen <samphippen@googlemail.com>
2011-09-22 21:54:58 +0100

    made main verbose aware with command line option
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
+#include <string.h>
 #include "square.h"
 
-int main() {
+int main(int argc, const char* argv[]) {
     latin_square* my_square = malloc(sizeof(latin_square));
-    init_latin_square("abcdefghijlkmnopqrstuvwxyz", my_square);
-    solve_latin_square(my_square);
+    init_latin_square("abcdefghijlkmnopqrstuvwxyz1234567890", my_square);
+    bool verbose = (argc >= 2 && strcmp(argv[1], "-v") == 0);
+    solve_latin_square(my_square, verbose);
+    if (!verbose) print_latin_square(my_square);
     return 0;
 }
