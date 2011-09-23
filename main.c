5d9717ad70f4b13fb13e04dbb10c11d0d1d69502
Sam Phippen <samphippen@googlemail.com>
2011-09-23 16:29:02 +0100

    added a command line argument for providing the alphabet
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "square.h"
 
 int main(int argc, const char* argv[]) {
-    latin_square* my_square = malloc(sizeof(latin_square));
-    init_latin_square("abcdefghijlkmnopqrstuvwxyz1234567890", my_square);
+    char* alphabet;
+
+    if (argc == 2 && strcmp(argv[1], "-v") != 0) {
+        alphabet = (char*) argv[1];
+    } else if (argc == 3) {
+        alphabet = (char*) argv[2];
+    } else {
+        alphabet = (char*) "abcdefghijklmnopqrstuvwxyz";
+    } 
+
     bool verbose = (argc >= 2 && strcmp(argv[1], "-v") == 0);
+
+    latin_square* my_square = malloc(sizeof(latin_square));
+    init_latin_square(alphabet, my_square);
     solve_latin_square(my_square, verbose);
     if (!verbose) print_latin_square(my_square);
     return 0;
